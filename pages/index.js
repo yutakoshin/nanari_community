070672a7d7e9727bc2b1175bd04336c5324256e3
yutakoshin <koshin757@gmail.com>
2021-09-20 17:19:55 +0900

    開発中(2021/09/20)_2

--- pages/index.js
+++ pages/index.js
@@ -29,17 +29,17 @@ export default function Home({ blog }) {
             </div>
           </div>
         </div>
-       <div className={styles.ImgContainer}>
+        <div className={styles.ImgContainer}>
           <img src="/static/S__6225945.jpg" alt="" />
-          <p>　もっとやりたいことがあるのに<br/>そんな簡単に進路なんて変えられない<br/>
-          <br/><br/>　　　　　　　　　　大学に行くのが辛い。</p>
+          <p>　もっとやりたいことがあるのに<br />そんな簡単に進路なんて変えられない<br />
+            <br /><br />　　　　　　　　　　大学に行くのが辛い。</p>
           <p></p>
         </div>
         <div className={styles.ImgContainer}>
           <img src="/static/S__6225930.jpg" alt="" />
-          <p>　　自分にはむいていない。そう思った。<br/>
-          　　　　でも、やめたら<br/>
-          ここまで頑張ってきた努力が無駄になるのかな。</p>
+          <p>　　自分にはむいていない。そう思った。<br />
+            でも、やめたら<br />
+            ここまで頑張ってきた努力が無駄になるのかな。</p>
         </div>
         <div className={styles.ImgContainer}>
           <img src="/static/S__6225932.jpg" alt="" />
@@ -48,33 +48,31 @@ export default function Home({ blog }) {
         <div className={styles.ImgContainer}>
           <img src="/static/S__6225944.jpg" alt="" />
           <p>nanariとは
-            <br/>Nanariは、大学で自分の夢を見失ってしまった学生
-            <br/>人間関係がこじれて大学にいけなくなってしまった学生<br/>
-            <br/>自分のゆありたいことがみつかったのに
-            <br/>大学生活に追われて動きが取れない学生<br/>
-            <br/>今の環境が自分に向いてないと気がついたのに、
-            <br/>これまでの積み上げを無駄にするのが怖かったり
-            <br/>経済状況が理由で
-            <br/>なかなか環境を変えることができない学生が
-            <br/>未来に向かって一歩を踏み出せるように支援をする団体です。<br/>
-            <br/>私たちは、自信をなくしている
-            <br/>あなたの才能、魅力を必ず伸ばせる自信があります。
+            <br />Nanariは、大学で自分の夢を見失ってしまった学生
+            <br />人間関係がこじれて大学にいけなくなってしまった学生<br />
+            <br />自分のゆありたいことがみつかったのに
+            <br />大学生活に追われて動きが取れない学生<br />
+            <br />今の環境が自分に向いてないと気がついたのに、
+            <br />これまでの積み上げを無駄にするのが怖かったり
+            <br />経済状況が理由で
+            <br />なかなか環境を変えることができない学生が
+            <br />未来に向かって一歩を踏み出せるように支援をする団体です。<br />
+            <br />私たちは、自信をなくしている
+            <br />あなたの才能、魅力を必ず伸ばせる自信があります。
           </p>
         </div>
-        
+
       </div >
       {/*ブログ表示↓*/}
       <div>
         <ul>
           {blog.map((blog) => (
-            <li key={blog.id}>
-              <Link href={`/blog/${blog.id}`}>
-                <a>{blog.title}</a>
-              </Link>
-            </li>
+            <Link href={`/blog/${blog.id}`}>
+              <a><img src={blog.image.url} alt="" /></a>
+            </Link>
           ))}
         </ul>
-      </div >
+      </div>
       {/*ブログ表示↑*/}
 
     </div>
