975ec594bc8f133194c36a4f3c640c8049679803
yutakoshin <koshin757@gmail.com>
2021-09-20 15:33:27 +0900

    開発中(2021/09/20)_1

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import { client } from "../libs/client";
 import Link from "next/link";
-
-import Header from './header'
+import Image from 'next/image'
+import Header from './header';
 import styles from '../styles/Home.module.css';
 
 export default function Home({ blog }) {
@@ -19,7 +19,7 @@ export default function Home({ blog }) {
         <div className="row">
           <div className="col">
             <div className="card">
-              <img className="card-img" src="/static/S__6045732.jpg" alt="" />
+              <img className="card-img img-responsive" src={'/static/S__6225935.jpg'} />
               <div className="card-img-overlay">
                 <div className={styles.TopText}>
                   <p className="text-white">こんなところで</p>
@@ -29,22 +29,41 @@ export default function Home({ blog }) {
             </div>
           </div>
         </div>
-        <div className="row">
-          <div className="col">
-            <div className={styles.PadNewInfomation}>
-              <p className={styles.LetterNews}>NEWS</p>
-              <p className={styles.LetterNewInfomation}>新着情報</p>
-            </div>
-          </div>
-          <div className="col">
-            <div className={styles.PadRenewal}>
-              <p className={styles.LetterRenewal}>2021年　サイトをリニューアルしました</p>
-            </div>
-          </div>
+       <div className={styles.ImgContainer}>
+          <img src="/static/S__6225945.jpg" alt="" />
+          <p>　もっとやりたいことがあるのに<br/>そんな簡単に進路なんて変えられない<br/>
+          <br/><br/>　　　　　　　　　　大学に行くのが辛い。</p>
+          <p></p>
         </div>
+        <div className={styles.ImgContainer}>
+          <img src="/static/S__6225930.jpg" alt="" />
+          <p>　　自分にはむいていない。そう思った。<br/>
+          　　　　でも、やめたら<br/>
+          ここまで頑張ってきた努力が無駄になるのかな。</p>
+        </div>
+        <div className={styles.ImgContainer}>
+          <img src="/static/S__6225932.jpg" alt="" />
+          <p>あれ、俺こんなことしたかったんだっけ。</p>
+        </div>
+        <div className={styles.ImgContainer}>
+          <img src="/static/S__6225944.jpg" alt="" />
+          <p>nanariとは
+            <br/>Nanariは、大学で自分の夢を見失ってしまった学生
+            <br/>人間関係がこじれて大学にいけなくなってしまった学生<br/>
+            <br/>自分のゆありたいことがみつかったのに
+            <br/>大学生活に追われて動きが取れない学生<br/>
+            <br/>今の環境が自分に向いてないと気がついたのに、
+            <br/>これまでの積み上げを無駄にするのが怖かったり
+            <br/>経済状況が理由で
+            <br/>なかなか環境を変えることができない学生が
+            <br/>未来に向かって一歩を踏み出せるように支援をする団体です。<br/>
+            <br/>私たちは、自信をなくしている
+            <br/>あなたの才能、魅力を必ず伸ばせる自信があります。
+          </p>
+        </div>
+        
       </div >
-
-{/*ブログ表示↓*/}
+      {/*ブログ表示↓*/}
       <div>
         <ul>
           {blog.map((blog) => (
@@ -56,7 +75,7 @@ export default function Home({ blog }) {
           ))}
         </ul>
       </div >
-{/*ブログ表示↑*/}
+      {/*ブログ表示↑*/}
 
     </div>
 
